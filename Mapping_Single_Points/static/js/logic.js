bcd80789174f9cf7ad04130066e14af5046e9028
Molly Sprecher <mollysprecher@Mollys-MacBook-Air.local>
2021-11-22 13:55:29 -0500

    files

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -4,20 +4,13 @@ console.log("working");
 //create map object w centre and zoom level 
 let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
-//get data from cities.js 
-let cityData = cities;
-
-//loop through cities array and create markers
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population/100000
-    }
-        )
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-    .addTo(map);
-})
-
+//add marker for LA, CA 
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300, 
+    color: "black", 
+    fillColor: "#ffffa1"
+}).addTo(map);
 
 //create tile layer that will be background of map 
 // We create the tile layer that will be the background of our map.
